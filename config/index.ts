1153959013f3dce055fe185e9362ede749d0f1ac
fitquick <141992941+fitquick@users.noreply.github.com>
2024-04-12 00:50:54 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -10,8 +10,8 @@ export const APP_INFO: AppInfo = {
   default_language: 'ja',
 }
 
-export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const isShowPrompt = true
+export const promptTemplate = 'あなたは優秀なアシスタントです。'
 
 export const API_PREFIX = '/api'
 
