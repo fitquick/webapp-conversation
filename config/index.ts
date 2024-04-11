24de8cc661aa0e35d955b144f8ebff473d19204a
fitquick <141992941+fitquick@users.noreply.github.com>
2024-04-12 04:17:43 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,8 @@
 import type { AppInfo } from '@/types/app'
 
-export const APP_ID = process.env.NEXT_PUBLIC_APP_ID as string
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string
-export const API_URL = process.env.NEXT_PUBLIC_API_URL as string
+export const NEXT_PUBLIC_APP_ID = process.env.NEXT_PUBLIC_APP_ID as string
+export const NEXT_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY as string
+export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL as string
 
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
