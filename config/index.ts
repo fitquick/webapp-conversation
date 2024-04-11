af9c99d7a1a9d62253dea79a1df28c485d8eef5a
fitquick <141992941+fitquick@users.noreply.github.com>
2024-04-12 02:49:05 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,9 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
-export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+
+export const APP_ID = process.env.APP_ID
+export const API_KEY = process.env.API_KEY
+export const API_URL = process.env.API_URL
+
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
@@ -12,9 +14,6 @@ export const APP_INFO: AppInfo = {
 
 export const isShowPrompt = true
 export const promptTemplate = 'あなたは優秀なアシスタントです。'
-
 export const API_PREFIX = '/api'
-
 export const LOCALE_COOKIE_NAME = 'locale'
-
 export const DEFAULT_VALUE_MAX_LEN = 48
